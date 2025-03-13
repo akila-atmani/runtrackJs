11111fbea8d440d63bd4bb5dcd05529d62ceb841
akila-atmani <akila.atmani@laplateforme.io>
2025-03-13 11:25:10 +0100

    job02

--- Jour03/job02/script.js
+++ Jour03/job02/script.js
@@ -1,15 +1,8 @@
 $(document).ready(function() {
-
-
-    $('#melangeimages').click(function() {
-        // Sélectionner les images avec la classe 'rainbow-image'
-        var images = $('#imagesContainer .rainbow-image');
-
-        images.sort(function() {
-            return 0.5 - Math.random(); // Mélange aléatoire
-        });
-
-       
-        $('#imagesContainer').html(images);
+    $("button").click(function() {
+        let images = $("#container img").toArray();
+        images.sort(() => Math.random() - 0.5);
+        $("#container").empty().append(images);
     });
 });
+
